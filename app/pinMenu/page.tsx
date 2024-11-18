2d4802011889fd66ea3e66ef3976150a5878389f
GRIN-96 <bnmnil96@gmail.com>
2024-11-18 12:55:20 +0900

    2024 1118 pin right menu 적용

--- app/pinMenu/page.tsx
+++ app/pinMenu/page.tsx
@@ -1,40 +1,68 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import Styles from "../page.module.scss";
+import { FiHome, FiMapPin, FiFolder, FiLock } from 'react-icons/fi';
 
-export default function Spotlight() {
-    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+const icons = [
+    {
+      shape: <FiHome />,
+    },
+    {
+      shape: <FiMapPin />,
+    },
+    {
+      shape: <FiFolder />,
+    },
+    {
+      shape: <FiLock />,
+    },
+]
 
-    const handleMouseMove = (event: React.MouseEvent) => {
-        setMousePosition({
-            x: event.clientX,
-            y: event.clientY,
-        });
-    };
+const MenuBar = ({obj}) => {
+    const icon = icons.map((x) => {
+        return x.shape;
+    })
+
+    const page = obj === undefined || obj === null ? icon[0] : "";
+
+    const [selectedMenu, setSelectedMenu] = useState(page);
+
+    useEffect(() => {
+        setSelectedMenu(page);
+    }, []);
+
+    const handleMouseOut = () => {
+        setSelectedMenu(page); // 현재 메뉴로 이동되도록 수정
+    }
+
 
     return (
-        // <main className={Styles.linkContainer}>
-        //     <Link href="./" className={Styles.link}>
-        //         뒤로가기
-        //     </Link>
-        // </main>
-        <div className={Styles.container} onMouseMove={handleMouseMove}>
-            <motion.div
-                className={Styles.spotlight}
-                style={{
-                    left: mousePosition.x,
-                    top: mousePosition.y,
-                }}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 0.3 }}
-            />
-            <div className={Styles.content}>
-                <h1>Welcome to the Spotlight</h1>
-                <p>Move your mouse to see the spotlight in action.</p>
-            </div>
-        </div>
+        <main className={Styles.linkContainer}>
+             <Link href="./" className={Styles.link}>
+                 뒤로가기
+             </Link>
+        
+            <motion.div className={Styles.menu_list}>
+                <div className={Styles.menu_item}>
+                    <ul>
+                        {icons.map((item, i) => {
+                            return (
+                            <li key={`${item.shape}-${i}`} onMouseOver={() => setSelectedMenu(item.shape)} //onMouseOut={handleMouseOut}
+                            >
+                                {item.shape}
+                                {item.shape == selectedMenu ? (
+                                <motion.div layoutId="underline" className={Styles.underline}></motion.div>
+                                ) : null}
+                            </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            </motion.div>
+        </main>
     );
 }
+
+export default MenuBar;
