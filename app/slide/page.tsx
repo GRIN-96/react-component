9059d99ca6401f114a326b6648eaf1506cb6ea4d
GRIN-96 <bnmnil96@gmail.com>
2024-11-20 00:24:55 +0900

    Comment: 페이지 넘김 이펙트 수정 turn.js > react-pageflip

--- app/slide/page.tsx
+++ app/slide/page.tsx
@@ -1,21 +1,17 @@
 "use client";
 
 import React, { useEffect } from "react";
-import $ from "jquery";
-import "turn.js";
 import Link from "next/link";
 import Styles from "../../app/page.module.scss";
+import HTMLFlipBook from "react-pageflip";
 
-const options = {
-    width: 800,
-    height: 600,
-    autoCenter: true,
-    display: "single",
-    acceleration: true,
-    elevation: 50,
-    gradients: !$.isTouch,
-    duration: 1500,
-};
+// const pages = [
+//     "/img/1.png",
+//     "/img/2.png",
+//     "/img/3.png",
+//     "/img/4.png",
+//     "/img/5.png",
+// ];
 
 const pages = [
     "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/01.jpg",
@@ -26,88 +22,53 @@ const pages = [
     "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/06.jpg",
 ];
 
-let initialX: number | null = null;
-let initialY: number | null = null;
+type SizeType = "fixed" | "stretch";
 
-function initTouch(e: TouchEvent | MouseEvent) {
-    initialX = e instanceof TouchEvent ? e.touches[0].clientX : e.clientX;
-    initialY = e instanceof TouchEvent ? e.touches[0].clientY : e.clientY;
-}
+const options = {
+    startPage: 0,
+    size: "stretch" as SizeType,
+    minWidth: 0,
+    maxWidth: 0,
+    minHeight: 0,
+    maxHeight: 0,
+    drawShadow: true,
+    flippingTime: 100,
+    usePortrait: true,
+    startZIndex: 0,
+    autoSize: true,
+    maxShadowOpacity: 0.5,
+    showCover: false,
+    mobileScrollSupport: false,
+    clickEventForward: true,
+    useMouseEvents: true,
+    swipeDistance: 100,
+    showPageCorners: false,
+    disableFlipByClick: false,
+};
 
 export default function PageSlide() {
-    useEffect(() => {
-        const bookElement = $("#book");
-
-        // `turn.js` 초기화
-        if (bookElement.length > 0) {
-            bookElement.turn(options);
-        }
-
-        // 스크롤 이벤트 추가
-        const handleScroll = (e: WheelEvent) => {
-            if (e.deltaY > 0) {
-                bookElement.turn("next");
-            } else {
-                bookElement.turn("previous");
-            }
-        };
-
-        // 마우스 및 터치 이벤트 설정
-        const handleMouseDown = (e: MouseEvent) => {
-            if (e.button === 0) {
-                initTouch(e);
-                const handleMouseMove = (moveEvent: MouseEvent) => swipeDirection(moveEvent, bookElement);
-
-                const handleMouseUp = () => {
-                    window.removeEventListener("mousemove", handleMouseMove);
-                    window.removeEventListener("mouseup", handleMouseUp);
-                };
-
-                window.addEventListener("mousemove", handleMouseMove);
-                window.addEventListener("mouseup", handleMouseUp);
-            }
-        };
-
-        // 스와이프 화면넘김
-        function swipeDirection(e: TouchEvent | MouseEvent) {
-            if (initialX !== null && initialY !== null) {
-                const currentX = e instanceof TouchEvent ? e.touches[0].clientX : e.clientX;
-                const currentY = e instanceof TouchEvent ? e.touches[0].clientY : e.clientY;
-            
-                const diffX = initialX - currentX;
-                const diffY = initialY - currentY;
-            
-                if (Math.abs(diffX) > Math.abs(diffY)) {
-                    if (diffX > 0) {
-                        bookElement.turn("next");
-                    } else {
-                        bookElement.turn("previous");
-                    }
-                }
-            
-                initialX = null;
-                initialY = null;
-            }
-        }
-
-        const handleTouchStart = (e: TouchEvent) => initTouch(e);
-        const handleTouchMove = (e: TouchEvent) => swipeDirection(e);
-
-        window.addEventListener("wheel", handleScroll);
-        window.addEventListener("mousedown", handleMouseDown);
-        window.addEventListener("touchstart", handleTouchStart);
-        window.addEventListener("touchmove", handleTouchMove);
-
-    }, []);
-
     return (
-        <main className={Styles.linkContainer}>
+        <main
+            className={Styles.linkContainer}
+            style={{ width: "1000px", height: "1000px" }}
+        >
             <Link href="./" className={Styles.back_button}>
                 뒤로가기
             </Link>
-            <div id="book" style={{ width: "800px", height: "600px", margin: "0 auto", top: "100px"}}>
+
+            <HTMLFlipBook
+                width={1000}
+                height={500}
+                className={""}
+                style={{}}
+                {...options}
+            >
                 {pages.map((page, index) => (
-                    <div key={index} style={{ width: "100%", height: "100%" }}>
+                    <div
+                        key={index}
+                        className={`page page-${index + 1}`} // 고유 클래스 추가
+                        style={{ width: "100%", height: "100%" }}
+                    >
                         <img
                             src={page}
                             alt={`Page ${index + 1}`}
@@ -115,7 +76,7 @@ export default function PageSlide() {
                         />
                     </div>
                 ))}
-            </div>
+            </HTMLFlipBook>
         </main>
     );
 }
